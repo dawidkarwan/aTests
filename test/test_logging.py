d652a457b090389986e7349f27568cee43f3b6c1
Dawid Karwan <karwandawid@gmail.com>
2022-09-21 23:03:35 +0200

    change assert to assert_that

--- test/test_logging.py
+++ test/test_logging.py
@@ -1,10 +1,8 @@
-
-
 from src.page_logging import LoggingPage
+from assertpy import assert_that
 
 
 def test_logging_test_arena(driver, url_test_arena):
     driver.get(url_test_arena)
     LoggingPage(driver).log_in()
-    assert driver.find_element("xpath", "//*[@class='user-info']").text.split()[0] == "Gall"
-
+    assert_that(driver.find_element("xpath", "//*[@class='user-info']").text).contains("Gall Anonim")
