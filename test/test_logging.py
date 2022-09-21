eb6eb38add05de3eeb6f01af75c20a779c0f6b0e
Dawid Karwan <karwandawid@gmail.com>
2022-09-21 22:57:30 +0200

    improve test_logging_test_arena with fixture

--- test/test_logging.py
+++ test/test_logging.py
@@ -1,12 +1,10 @@
-from selenium import webdriver
+
 
 from src.page_logging import LoggingPage
 
-driver = webdriver.Chrome()
 
-
-def test_logging_test_arena():
-    driver.get("http://demo.testarena.pl/")
+def test_logging_test_arena(driver, url_test_arena):
+    driver.get(url_test_arena)
     LoggingPage(driver).log_in()
     assert driver.find_element("xpath", "//*[@class='user-info']").text.split()[0] == "Gall"
-    driver.quit()
+
