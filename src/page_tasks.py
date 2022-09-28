a07a9c9cd405793396741dfb8acd4720caa3cc8d
Dawid Karwan <karwandawid@gmail.com>
2022-09-28 21:30:45 +0200

    change tests catalog name, add test left nav

---
+++ src/page_tasks.py
@@ -0,0 +1,5 @@
+from selenium.webdriver.common.by import By
+
+
+class TasksLocators:
+    TITLE = (By.XPATH, '//h1[@class="content_title"]')
--- tests/conftest.py
+++ tests/conftest.py
@@ -2,6 +2,8 @@ import pytest
 from selenium.webdriver import Chrome
 from selenium.webdriver.remote.webdriver import WebDriver
 
+from src.page_logging import LoggingPage
+
 
 @pytest.fixture
 def driver() -> WebDriver:
@@ -13,3 +15,9 @@ def driver() -> WebDriver:
 @pytest.fixture(scope="session")
 def url_test_arena() -> str:
     return "http://demo.testarena.pl/"
+
+
+@pytest.fixture()
+def log_in_to_test_arena(driver, url_test_arena):
+    driver.get(url_test_arena)
+    LoggingPage(driver).log_in()
