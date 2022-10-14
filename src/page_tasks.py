b7a5bda16d9e0e206e4fef2a40054cb763ed98dc
Dawid Karwan <karwandawid@gmail.com>
2022-10-14 19:21:14 +0200

    Move methods from test to page

--- src/page_tasks.py
+++ src/page_tasks.py
@@ -2,6 +2,8 @@ from dataclasses import dataclass
 
 from selenium.webdriver.common.by import By
 
+from src.commons.page_base import BasePage
+
 
 class TasksLocators:
     TITLE = (By.XPATH, '//h1[@class="content_title"]')
@@ -22,7 +24,7 @@ class TasksLocators:
 
 
 @dataclass
-class PageTasksDataClass:
+class TasksDataClass:
     title: str
     description: str
     env: str
@@ -31,3 +33,19 @@ class PageTasksDataClass:
     assignee_to: str
     tag: str
     priority: str
+
+
+class TasksPage(BasePage):
+
+    def fill_new_task_fields(self, params: TasksDataClass):
+        self.driver.find_element(*TasksLocators.BUTTON_ADD_TASK).click()
+        self.driver.find_element(*TasksLocators.INPUT_TITLE).send_keys(params.title)
+        self.driver.find_element(*TasksLocators.INPUT_DESCRIPTION).send_keys(params.description)
+        self.driver.find_element(*TasksLocators.INPUT_ENV).send_keys(params.env)
+        self.driver.find_element(*TasksLocators.INPUT_VERSION).send_keys(params.version)
+        self.driver.find_element(*TasksLocators.DDL_PRIORITY).click()
+        self.driver.find_element(*TasksLocators.priority_option(params.priority)).click()
+        self.driver.find_element(*TasksLocators.INPUT_DUE_DATE).send_keys(params.due_date)
+        self.driver.find_element(*TasksLocators.INPUT_ASSIGNEE_TO).send_keys(params.assignee_to)
+        self.driver.find_element(*TasksLocators.INPUT_TAG).send_keys(params.tag)
+        self.driver.find_element(*TasksLocators.BUTTON_SAVE).click()
--- tests/test_tasks.py
+++ tests/test_tasks.py
@@ -1,7 +1,7 @@
 import pytest
 
 from src.page_left_nav import LeftNavPage, LeftNavData
-from src.page_tasks import TasksLocators, PageTasksDataClass
+from src.page_tasks import TasksDataClass, TasksPage
 from src.utils.get_test_data import get_test_data
 
 
@@ -9,19 +9,11 @@ class TestTasks:
 
     @pytest.fixture(autouse=True)
     def setup(self, driver):
+        self.driver = driver
         self.left_nav = LeftNavPage(driver)
+        self.tasks = TasksPage(driver)
 
-    @pytest.mark.parametrize('params', get_test_data('tasks', PageTasksDataClass))
-    def test_add_task(self, driver, params: PageTasksDataClass):
+    @pytest.mark.parametrize('params', get_test_data('tasks', TasksDataClass))
+    def test_add_task(self, params: TasksDataClass):
         self.left_nav.go_to_tab(LeftNavData.TASK.value)
-        driver.find_element(*TasksLocators.BUTTON_ADD_TASK).click()
-        driver.find_element(*TasksLocators.INPUT_TITLE).send_keys(params.title)
-        driver.find_element(*TasksLocators.INPUT_DESCRIPTION).send_keys(params.description)
-        driver.find_element(*TasksLocators.INPUT_ENV).send_keys(params.env)
-        driver.find_element(*TasksLocators.INPUT_VERSION).send_keys(params.version)
-        driver.find_element(*TasksLocators.DDL_PRIORITY).click()
-        driver.find_element(*TasksLocators.priority_option(params.priority)).click()
-        driver.find_element(*TasksLocators.INPUT_DUE_DATE).send_keys(params.due_date)
-        driver.find_element(*TasksLocators.INPUT_ASSIGNEE_TO).send_keys(params.assignee_to)
-        driver.find_element(*TasksLocators.INPUT_TAG).send_keys(params.tag)
-        driver.find_element(*TasksLocators.BUTTON_SAVE).click()
+        self.tasks.fill_new_task_fields(params)
